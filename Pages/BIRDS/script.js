24ba05f7bc6dd2face5d138d70cf753d3d02a4ec
Pramod Singh Thakuri <singhthakuri2005@gmail.com>
2026-05-28 08:29:13 +0545

    edit in birds section

--- Pages/BIRDS/script.js
+++ Pages/BIRDS/script.js
@@ -1,13 +1,51 @@
-function filterAnimals(type) {
-  let cards = document.querySelectorAll(".card");
+// ================= FILTER BUTTON =================
 
-  cards.forEach((card) => {
-    if (type === "all") {
-      card.style.display = "block";
-    } else if (card.classList.contains(type)) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
-}
+const buttons = document.querySelectorAll(".filter-btn");
+
+const cards = document.querySelectorAll(".bird-card");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        // Remove active class
+
+        buttons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        // Add active class
+
+        button.classList.add("active");
+
+        // Get filter value
+
+        const filter = button.getAttribute("data-filter");
+
+        // Filter cards
+
+        cards.forEach(card => {
+
+            if(filter === "all"){
+
+                card.style.display = "block";
+
+            }
+
+            else if(card.classList.contains(filter)){
+
+                card.style.display = "block";
+
+            }
+
+            else{
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
